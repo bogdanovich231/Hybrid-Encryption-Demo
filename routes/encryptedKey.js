0a76f89ca415038075d7dbe830d42a21fa76b10b
Tatiana <96041733+bogdanovich231@users.noreply.github.com>
2025-12-02 01:39:18 +0100

    fix: change method

--- routes/encryptedKey.js
+++ routes/encryptedKey.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const keyController = require("../controllers/keyController");
 
-router.post("/get-secret", keyController.encrypted);
+router.get("/get-secret", keyController.encrypted);
 
 module.exports = router;
